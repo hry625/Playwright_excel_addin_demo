b4e12a7f8b21053f6fe6de5a65c5e728114d1428
flyf <hry87625@gmail.com>
2023-11-27 12:33:22 +0100

    add force:true flag

--- playwright_demo/tests/example.spec.ts
+++ playwright_demo/tests/example.spec.ts
@@ -3,11 +3,11 @@ import { test, expect, chromium } from '@playwright/test';
 test('click Show taskpane button', async ({ }) => {
   const context = await chromium.launch({ headless: false })
   const page = await context.newPage()
-  await page.goto('path to an empty xlsx file');
+  await page.goto('path/To/An Empty Excel file');
   await page.pause()
   //  upload manifest file
   await expect(page.getByRole("button").filter({ hasText: "Taskpane" })).toBeHidden()
-  await page.getByRole("button").filter({ hasText: "Taskpane" }).click()
+  await page.getByRole("button").filter({ hasText: "Taskpane" }).click({ force: true })
 });
 
 
